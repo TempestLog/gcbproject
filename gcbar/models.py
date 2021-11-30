6035279744512ecf836f7c83625b0830eccfa242
TempestLog <atabesobright@gmail.com>
2021-11-30 23:26:17 +0000

    pushing to heroku

--- gcbar/models.py
+++ gcbar/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-from datetime import date, time
+
 
 # Create your models here.
-class Workers(models.Model):
-    date = models.DateField (default=date.now, blank=True)
-    time = models.TimeField(max_length=100)
-    arrivaltime = models.TimeField(auto_now=False)
