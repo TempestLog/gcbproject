5ef2f055680540507f35969dc9be16bc309c3f80
TempestLog <atabesobright@gmail.com>
2021-12-01 23:33:45 +0000

    db tables

--- gcbar/admin.py
+++ gcbar/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import Workers, Dates
 
 
 # Register your models here.
+admin.site.register(Workers)
+admin.site.register(Dates)
--- gcbar/models.py
+++ gcbar/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class Workers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    arrivaltime = models.TimeField()
 
+class Dates(models.Model):
+    workday = models.DateField(default=datetime.today)
+    start_time = models.TimeField()
+    employee = models.ManyToManyField(Workers, blank=True)
